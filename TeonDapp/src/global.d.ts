df4bf06656442aa43f83744561818fc514c88228
Teon x <teonx7@gmail.com>
2025-03-31 15:14:11 +0100

    Update global.d.ts

--- TeonDapp/src/global.d.ts
+++ TeonDapp/src/global.d.ts
@@ -2,6 +2,11 @@ import { ethers } from 'ethers';
 
 declare global {
   interface Window {
-    ethereum?: ethers.Eip1193Provider;
+    ethereum?: ethers.Eip1193Provider & {
+      isMetaMask?: boolean;
+      request: (request: { method: string; params?: any[] }) => Promise<any>;
+      on: (event: string, callback: (...args: any[]) => void) => void;
+      removeListener: (event: string, callback: (...args: any[]) => void) => void;
+    };
   }
 }
