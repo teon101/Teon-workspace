f5135e686080bc3a0f8f43667d29b33067c04a1f
Teon x <teonx7@gmail.com>
2025-03-31 14:53:03 +0100

    Update web3.ts

--- TeonDapp/src/utils/web3.ts
+++ TeonDapp/src/utils/web3.ts
@@ -1,85 +1,46 @@
 import { ethers } from "ethers";
 
-const contractAddress = "https://shannon-explorer.somnia.network/tx/0xde15618339f754841fa2d354a74f395f63b14b5fd29eb98a8b4e235a0789e7dd"; // Replace with your deployed contract address
+const contractAddress = "0xde15618339f754841fa2d354a74f395f63b14b5fd29eb98a8b4e235a0789e7dd"; // Replace with your actual contract address
+
 const contractABI = [
-  [
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "oldName",
-          "type": "string"
-        },
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "newName",
-          "type": "string"
-        }
-      ],
-      "name": "NameChanged",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_newName",
-          "type": "string"
-        }
-      ],
-      "name": "changeName",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "greet",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "name",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "owner",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
+  { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": false, "internalType": "string", "name": "oldName", "type": "string" },
+      { "indexed": false, "internalType": "string", "name": "newName", "type": "string" }
+    ],
+    "name": "NameChanged",
+    "type": "event"
+  },
+  {
+    "inputs": [{ "internalType": "string", "name": "_newName", "type": "string" }],
+    "name": "changeName",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "greet",
+    "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "stateMutability": "view",
+    "type": "function"
+  }
 ];
 
 export async function getContract(signer: ethers.Signer | null = null) {
